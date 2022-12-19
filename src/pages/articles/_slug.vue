31eb401481a90af70037e484aaf950ae3ef484cf
xFlest <mail@fle.st>
2022-12-20 03:22:22 +0900

    .

--- src/pages/articles/_slug.vue
+++ src/pages/articles/_slug.vue
@@ -41,7 +41,7 @@ export default {
         {
           hid: 'og:image',
           name: 'og:image',
-          content: this.article.image
+          content: `/image/${slug}/${article.image}`
         },
         {
           hid: 'og:description',
